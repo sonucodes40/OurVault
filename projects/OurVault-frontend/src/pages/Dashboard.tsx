490631c3275c7c91cb7c3ab6a031518396cc52f1
sonucodes40 <sonukumar254585@gmail.com>
2026-04-04 15:40:40 +0530

    created page setup

---
+++ projects/OurVault-frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className='text-xl fond-bold text-cyan-600'>
+      Dashboard
+    </div>
+  )
+}
+
+export default Dashboard
